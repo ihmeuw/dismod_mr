efdac979a566c9959306e54dea3f694395329529
A. Flaxman <abie@alum.mit.edu>
2013-12-09 12:12:45 -0800

    ENH: add license

--- dismod_mr/model/likelihood.py
+++ dismod_mr/model/likelihood.py
@@ -1,3 +1,22 @@
+
+
+# Copyright 2008-2012 University of Washington
+# 
+# This file is part of DisMod-MR.
+# 
+# DisMod-MR is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DisMod-MR is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with DisMod-MR.  If not, see <http://www.gnu.org/licenses/>.
+
 """ Several rate models"""
 
 import pylab as pl
